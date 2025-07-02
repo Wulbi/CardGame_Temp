2035af89b053f8fc4be082459811d2ebddeb263f
DongHyeon Kim <younghero2612@gmail.com>
2025-07-03 01:02:36 +0900

    First Version
    Reference: "Slay The Spire in UNITY (Ep. 1) - Setup & Card Prefab" https://www.youtube.com/watch?v=rgsp9pb0Oi0

---
+++ Assets/Scripts/Views/CardView.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    [SerializeField] private GameObject wrapper;
+    [SerializeField] private TMP_Text mana;
+    [SerializeField] private TMP_Text name;
+    [SerializeField] private TMP_Text desc;
+    [SerializeField] private SpriteRenderer cardImage;
+
+
+}
